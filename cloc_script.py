37efed1e2f3c9caf16e0d5f202b4a02a4e6951a4
Robin Dao <daorob@gmail.com>
2022-01-16 00:48:18 -0600

    Download repo zip file with wget.

--- cloc_script.py
+++ cloc_script.py
@@ -1,4 +1,10 @@
 import sys
+import wget
 
 # retrieve github repo zip file link from first command line argument
 url = sys.argv[1]
+
+# download repo zip file
+filename = wget.download(url)
+print(chr(10))
+print('GitHub repository zip file downloaded.\n')
